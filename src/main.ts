0f51e1033b9f200e85b6210a72886d337f931cb8
Emerson <emerson.matosokl10@gmail.com>
2024-05-19 18:41:20 -0300

    feat(backend): Add swagger e dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import('dotenv').then((dotenv) => {
+  dotenv.config();
+});
+
+const port = process.env.PORT || 3000;
+
+/*
+ * rota barra
+ * rota / para swagger
+ *
+ * */
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Catalogo de foilmes API')
+    .setDescription('API para catalogo de filmes com NestJS e TypeORM')
+    .setVersion('1.0')
+    .addTag('filmes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log('Press CTRL + C to stop the server');
+    console.log('env:', process.env.NODE_ENV || 'development');
+  });
 }
 bootstrap();
